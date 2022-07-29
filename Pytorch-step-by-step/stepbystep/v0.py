439f5939fd3f96d3815909fb36116204bc8511eb
Rit-ctrl <rithic40@gmail.com>
2022-07-29 20:12:16 +0530

    Chap 3 Logistic

--- Pytorch-step-by-step/stepbystep/v0.py
+++ Pytorch-step-by-step/stepbystep/v0.py
@@ -8,12 +8,18 @@ plt.style.use('fivethirtyeight')
 
 class StepByStep(object):
 
-    def __init__(self,model,loss_fn,optimizer):
+    def __init__(self,model,loss_fn,optimizer,mps = False):
 
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if mps and torch.backends.mps.is_available():
+            # print(mps,torch.backends.mps.is_available())
+            
+            self.device = 'mps'
+        else:
+            # print(mps,torch.backends.mps.is_available())
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model.to(self.device)
 
         self.train_loader = None
